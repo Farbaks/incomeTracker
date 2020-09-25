fd4cd23d0ed9f44ce9e11fe083f2278dddbb8fc6
Farbaks <faroukbakre@yahoo.com>
2020-09-25 15:26:32 +0100

    sharing and image picking modules

--- src/app/pages/job-detail/job-detail.page.ts
+++ src/app/pages/job-detail/job-detail.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-job-detail',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobDetailPage implements OnInit {
 
-  constructor() { }
+  constructor(private socialSharing: SocialSharing) { }
 
   ngOnInit() {
   }
 
+  share() {
+    this.socialSharing.share(
+      'Seed Block Algorithm sample pdfs',
+      'Seed Block Algorithm',
+      [
+        'http://www.ijstm.com/images/short_pdf/1431859738_525.pdf',
+        'http://www.chennaisunday.com/IEEE%202013%20Java%20Basepaper/Seed%20Block%20Algorithm%20A%20Remote%20Smart%20Data%20Back-up%20Technique%20for%20Cloud%20Computing.pdf'
+      ],
+      'http://www.ijfrcsce.org/download/browse/Volume_4/April_18_Volume_4_Issue_4/1524130771_19-04-2018.pdf');
+  }
 }
--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ImagePicker } from '@ionic-native/image-picker/ngx';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  constructor(private imagePicker: ImagePicker) { }
 
   ngOnInit() {
+  }
+  hasReadPermission() {
+    this.imagePicker.hasReadPermission().then((result) => {
+      if(result == false){
+        this.requestReadPermission();
+      }
+    })
+  }
+
+  requestReadPermission() {
+    // no callbacks required as this opens a popup which returns async
+    this.imagePicker.requestReadPermission();
+  }
+  choosePicture() {
+    let options = {
+      maximumImagesCount: 10,
+      width: 800,
+      outputType: 1
+    }
+    this.imagePicker.getPictures(options).then((results) => {
+      for (var i = 0; i < results.length; i++) {
+        console.log('Image URI: ' + results[i]);
+      }
+    }, (err) => { });
   }
 
 }
